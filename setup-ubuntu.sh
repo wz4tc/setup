48fa864017d85fb9ce8e59e96f1b6a8eca2ea672
elliott <elliott@aws.etolli.com>
2015-11-12 07:15:11 +0000

    color full

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -3,22 +3,29 @@
 BASEDIR=$(dirname $0)
 USER=$(whoami)
 HOME=$(echo ~)
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 sudo apt-get install git tmux htop
 
 if [ -e $HOME/.tmux ];
 then
-	echo "Tmux config Installed"
+	printf "${RED}Tmux config already Installed${NC}\n"
 else
-	git clone https://github.com/tony/tmux-config.git ~/.tmux
-	ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+	git clone https://github.com/tony/tmux-config.git $HOME/.tmux
+	ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
+	printf "${GREEN}Tmux conf complete${NC}\n"
 fi
 
 if [ -e $HOME/.vim/bundle ];
 then
-	echo "Vim Bundle Installed"
+	printf "${RED}Vim Bundle already Installed${NC}\n"
 else
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	cp $BASEDIR/.vimrc ~/.vimrc
+	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+	cp $BASEDIR/.vimrc $HOME/.vimrc
+	vim -c "PluginInstall" -c "qall"
+	printf "${GREEN}Vim Bundle complete${NC}\n"
 fi
 
 
